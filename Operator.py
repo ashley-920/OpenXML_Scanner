990abf30d54f2830a6fb831b1fa916cbcce8759d
Ashley S <ashley-920@hotmail.com>
2014-05-27 15:42:10 +0800

    delete files

--- Operator.py
+++ Operator.py
@@ -138,53 +138,5 @@ class Operator:
             md5.update(data)
         return md5.hexdigest() 
 
-    # def write_report(self,sample):
-    #     summary="\n################### Report Summary ###################\n"
-    #     summary+="Sample File Path:"+sample.file_path+"\n"
-    #     summary+="Sample File Name:"+sample.file_name+"\n"
-    #     summary+="Sample Directory:"+sample.sample_dir+"\n"
-    #     summary+="Sample Extract File Directory:"+sample.extract_file_dir+"\n"
-    #     summary+="Sample File Type:"+sample.file_type+"\n"
-    #     summary+="Sample File List:\n"
-    #     c=0
-    #     for file in sample.file_list:
-    #         summary+="  ["+str(c)+"]"+file+"\n"
-    #         c+=1
-    #     if not sample.extension_List == None:
-    #         summary+="Sample Extension List:\n"         
-    #         for ext in sample.extension_List:
-    #             summary+="  *"+ext+"\n"     
-    #     if not sample.bin_file_list == None:
-    #         summary+="Sample Binary List:\n"            
-    #         for bin in sample.bin_file_list:
-    #             summary+="  *"+bin+"\n"
-        
-    #     if not sample.file_contain_flash == None:
-    #         summary+="SWF File Directory:"+sample.swf_dir+"\n"
-    #         if not sample.flash_obj_list == None:
-    #             summary+="Flash Object Number:"+str(len(sample.flash_obj_list))+"\n"
-    #             c=0
-    #             for flash in sample.flash_obj_list:
-    #                 summary+="Flash Object ["+str(c)+"]\n"
-    #                 summary+="  File Name: "+flash.file_name+"\n"
-    #                 summary+="  File Path: "+flash.file_path+"\n"
-    #                 summary+="  File Size: "+str(flash.file_size)+"\n"
-    #                 summary+="  Flash Header Type: "+flash.swf_type+"\n"
-    #                 summary+="  ActionScript File Name: "+flash.as_file_name+"\n"
-    #                 summary+="  ActionScript File Path: "+flash.as_file_path+"\n"
-    #                 c+=1        
-    #     if not sample.vba_dir == None:
-    #         summary+="VBA File Path:"+sample.vba_path+"\n"
-    #         summary+="VBA File Directory:"+sample.VBA_dir+"\n"
-        
-    #     summary+="\nIOM of this sample:"+str(sample.IOM)+"\n\n"
-    #     summary+="#######################################################\n"
-    #     summary+="Report Detail:\n"
-
-    #     sample.report=summary+sample.report
-    #     report_file_name=sample.file_name+" Scanning Report.txt"
-    #     output_path=os.path.join(sample.sample_dir,report_file_name)
-    #     f=open(output_path,'w')
-    #     f.write(sample.report)
-    #     f.close()
+    
 
